1faa4bfced9fbd60f5eb460e8083bc9345afc98b
Anshul Singh <anshul.singh.rules.haha@gmail.com>
2020-06-22 13:06:17 -0700

    Create best-time-to-buy-and-sell-stock.java

---
+++ best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int maxProfit(int prices[]) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int i : prices) {
+            if (i < minPrice) {
+                minPrice = i;
+            } else if (i-minPrice > maxProfit) {
+                maxProfit = i - minPrice;
+            }
+        }
+        return maxProfit;
+    }
+}

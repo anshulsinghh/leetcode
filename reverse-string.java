ea1af82d37652e356db3d31cc26fd044304a6e93
Anshul Singh <anshul.singh.rules.haha@gmail.com>
2020-06-23 09:04:49 -0700

    Create reverse-string.java

---
+++ reverse-string.java
@@ -0,0 +1,10 @@
+class Solution {
+    public void reverseString(char[] s) {
+        if (s.length <= 1) return;
+        for (int i = 0, j = s.length-1; i <= j; i++, j--) {
+            char tmp = s[i];
+            s[i] = s[j];
+            s[j] = tmp;
+        }
+    }
+}

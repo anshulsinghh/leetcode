694caee0b194bcb1718f9c379d7b57d5df6fb366
Anshul Singh <anshul.singh.rules.haha@gmail.com>
2020-07-05 15:39:55 -0700

    Update same-tree.java

--- same-tree.java
+++ same-tree.java
@@ -1,10 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if ((p == null || q == null) && p != q) return false;
         if (p == null && q == null) return true;
-        if (p.val != q.val) return false;
-        if (!isSameTree(p.left, q.left)) return false;
-        if (!isSameTree(p.right, q.right)) return false;
-        return true;
+        if (p == null || q == null) return false;
+        if (p.val == q.val)
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        return false;
     }
 }

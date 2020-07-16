e5c2e35bced7606c4a83d94fb1e2be2f04d29f13
Anshul Singh <anshul.singh.rules.haha@gmail.com>
2020-07-16 13:50:04 -0700

    Create path-sum-2.java

---
+++ path-sum-2.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private List<List<Integer>> resultList = new ArrayList<List<Integer>>();
+    
+    public void pathSumInner(TreeNode root, int sum, Stack<Integer>path) {
+        path.push(root.val);
+        if(root.left == null && root.right == null)
+            if(sum == root.val) resultList.add(new ArrayList<Integer>(path));
+        if(root.left!=null) pathSumInner(root.left, sum-root.val, path);
+        if(root.right!=null)pathSumInner(root.right, sum-root.val, path);
+        path.pop();
+    }
+    
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        if(root==null) return resultList;
+        Stack<Integer> path = new Stack<Integer>();
+        pathSumInner(root, sum, path);
+        return resultList;
+    }
+}

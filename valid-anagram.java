f3b31ba0f90d829f31d21e2a3c0124d653255c40
Anshul Singh <anshul.singh.rules.haha@gmail.com>
2020-07-17 08:44:28 -0700

    Update valid-anagram.java

--- valid-anagram.java
+++ valid-anagram.java
@@ -1,22 +1,18 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        HashMap<Character, Integer> sMap = new HashMap<Character, Integer>();
-        for (int i = 0; i < s.length(); i++) {
-            if (sMap.containsKey(s.charAt(i))) {
-                sMap.put(s.charAt(i), sMap.get(s.charAt(i)) + 1);
-            } else {
-                sMap.put(s.charAt(i), 1);
-            }
+        HashMap<Character, Integer> sMap = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            if (sMap.containsKey(c)) sMap.replace(c, sMap.get(c)+1);
+            else sMap.put(c, 1);
         }
         
-        for (int i = 0; i < t.length(); i++) {
-            if (!sMap.containsKey(t.charAt(i))) return false;
+        for (char c : t.toCharArray()) {
+            if (sMap.containsKey(c)) sMap.replace(c, sMap.get(c)-1);
+            else return false;
             
-            sMap.put(t.charAt(i), sMap.get(t.charAt(i)) - 1);
+            if (sMap.get(c) == 0) sMap.remove(c);
         }
         
-        for (int i : sMap.values()) { if(i != 0) return false; }
-        
-        return true;
+        return sMap.isEmpty();
     }
 }
